ef83d759a49ae55d66e18e727ddc3ebc33d8f594
CANIS ALISE p1616229 <p1616229@univ-lyon1.fr>
2017-06-26 11:40:53 +0200

    ajout timeline.vue et Tweet.vue

---
+++ frontend/src/components/Timeline.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="timeline">
+    <ul>
+      <li v-for="tweet in tweets"><tweet :tweet="tweet"/></li>
+    </ul>
+  </div>
+</template>
+
+<script>
+var tweet1 = {auteur: 'auteur1', contenu: 'voilà un super contenu !'}
+var tweet2 = {auteur: 'auteur2', contenu: 'Wouah tip top !'}
+var tweet3 = {auteur: 'auteur3', contenu: 'On s\'éclate sur Twitter yahou !'}
+import Tweet from './Tweet'
+export default {
+  name: 'timeline',
+  components: {Tweet},
+  data () {
+    return {
+      tweets: [tweet1, tweet2, tweet3]
+    }
+  }
+}
+</script>
+
+
+<style scoped>
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: block;
+  margin: 0 10px;
+}
+</style>
---
+++ frontend/src/components/Tweet.vue
@@ -0,0 +1,12 @@
+<template>
+  <div class="tweet">
+    {{ tweet.auteur }} dit : {{ tweet.contenu }}
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'tweet',
+  props: ['tweet']
+}
+</script>
